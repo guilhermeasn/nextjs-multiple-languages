72a045dd3742295f0b987040a410afad85ee53ba
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-04-18 20:28:35 -0300

    improved change language component

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,44 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import languages from '../languages/config.json';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
 
-        <Head>
-            <title>Next App Multi Languages</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+    const router = useRouter();
+    
+    function changeLanguage(language) {
+        const { pathname, asPath, query } = router;
+        router.push({ pathname, query }, asPath, { locale: language });
+    }
 
-        <Component {...pageProps} />
+    return (
+        <>
 
-    </>
-  
-  );
+            <Head>
+                <title>Next App Multi Languages</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <Component {...pageProps} />
+            </main>
+
+            <footer>
+                <select value={ pageProps.locale } onChange={ selected => changeLanguage(selected.target.value) }>
+                    { Object.entries(languages).map(([ key, description ]) => (
+                        <option key={ key } value={ key }>
+                            🌐&nbsp;{ description }
+                        </option>
+                    )) }
+                </select>
+            </footer>
+
+        </>
+    
+    );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,18 @@
-import { useRouter } from 'next/router';
-import styles from '../styles/Home.module.css';
+export default function Home({ content }) {
 
-export default function Home({ content, languages, locale }) {
-
-    const router = useRouter();
-    
-    function changeLanguage(language) {
-        const { pathname, asPath, query } = router;
-        router.push({ pathname, query }, asPath, { locale: language });
-    }
-  
     return (
-
         <>
-
-            <main className={styles.main}>
-                <h1>{ content.title }</h1>
-                <p>{ content.body }</p>
-            </main>
-
-            <footer className={styles.footer}>
-                <select value={ locale } onChange={ selected => changeLanguage(selected.target.value) } className={styles.select}>
-                    { Object.entries(languages).map(([ key, description ]) => (
-                        <option key={ key } value={ key }>
-                            🌐&nbsp;{ description }
-                        </option>
-                    )) }
-                </select>
-            </footer>
-
+            <h1>{ content.title }</h1>
+            <p>{ content.body }</p>
         </>
-
-  );
+    );
 
 }
 
 export const getStaticProps = async ({ locale }) => {
 
-
     return {
-        props:{
-            content: require(`../languages/${locale}.json`)['home'],
-            languages: require('../languages/locales.json'),
-            locale
-        }
+        props: { content: require(`../languages/${locale}.json`)['home'], locale }
     }
 
 }
