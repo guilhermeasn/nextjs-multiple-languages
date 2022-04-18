e14bab2386d25031b95139cfd6891f17f4c9c5fb
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-04-18 20:48:41 -0300

    error 404 multiple languages

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,17 @@
-export default function Custom404() {
+export default function Custom404({ content }) {
 
-    return <h2 style={ {color: '#400'} }>404 | Page not found!</h2>
+    return (
+        <h2 style={ {color: '#400'} }>
+            404 | { content.http404 }
+        </h2>
+    );
+
+}
+
+export const getStaticProps = async ({ locale }) => {
+
+    return {
+        props: { content: require(`../languages/${locale}.json`)['error'], locale }
+    }
 
 }
